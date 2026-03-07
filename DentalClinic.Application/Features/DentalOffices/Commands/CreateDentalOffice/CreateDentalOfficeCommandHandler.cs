ed38d657737db0110eb903dd882182dd8f57a644
Miraj <Miraj@DESKTOP-PIQRSMH>
2026-03-07 23:52:15 +0530

    feat: custom handler added and fluent validation added

---
+++ DentalClinic.Application/Features/DentalOffices/Commands/CreateDentalOffice/CreateDentalOfficeCommandHandler.cs
@@ -0,0 +1,53 @@
+﻿using DentalClinic.Application.Contracts.Persistence;
+using DentalClinic.Application.Contracts.Repositories;
+using DentalClinic.Application.Exceptions;
+using DentalClinic.Domain.Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentalClinic.Application.Features.DentalOffices.Commands.CreateDentalOffice
+{
+    public class CreateDentalOfficeCommandHandler
+    {
+        private readonly IDentalOfficeRepository _dentalOfficeRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IValidator<CreateDentalOfficeCommand> _validator;
+
+        public CreateDentalOfficeCommandHandler(IDentalOfficeRepository dentalOfficeRepository, IUnitOfWork unitOfWork, IValidator<CreateDentalOfficeCommand> validator)
+        {
+            _dentalOfficeRepository = dentalOfficeRepository;
+            _unitOfWork = unitOfWork;
+            _validator = validator;
+        }
+
+        public async Task<Guid> Handle(CreateDentalOfficeCommand command)
+        {
+            var validationResult = await _validator.ValidateAsync(command);
+            if (validationResult.IsValid) 
+            {
+                await _unitOfWork.Rollback();
+                throw new CustomValidationException(validationResult);
+            }
+
+            var dentalOffice = new DentalOffice(command.Name);
+
+            try
+            {
+                await _dentalOfficeRepository.AddAsync(dentalOffice);
+                await _unitOfWork.Commit();
+                return dentalOffice.Id;
+
+            }
+            catch (Exception ex) 
+            {
+                await _unitOfWork.Rollback();
+                throw ex;
+            }
+
+        }
+    }
+}
---
+++ DentalClinic.Application/Features/DentalOffices/Commands/CreateDentalOffice/CreateDentalOfficeCommandValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DentalClinic.Application.Features.DentalOffices.Commands.CreateDentalOffice
+{
+    public class CreateDentalOfficeCommandValidator : AbstractValidator<CreateDentalOfficeCommand>
+    {
+        public CreateDentalOfficeCommandValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("The Name field is required.")
+                .MaximumLength(100).WithMessage("The Name field must not exceed 100 characters.");
+        }
+
+    }
+}
