2e509452dd2cf3d21911207be4033bc003ec1fcc
Miraj <Miraj@DESKTOP-PIQRSMH>
2026-03-12 12:36:51 +0530

    feat: connected to db local

---
+++ DentalClinic.API/Program.cs
@@ -0,0 +1,22 @@
+using DentalClinic.Application;
+using DentalClinic.Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddPersistenceServices();
+builder.Services.AddApplicationServices();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
--- DentalClinic.Persistence/RegisterPersistenceServices.cs
+++ DentalClinic.Persistence/RegisterPersistenceServices.cs
@@ -3,6 +3,7 @@ using DentalClinic.Application.Contracts.Repositories;
 using DentalClinic.Persistence.Repositories;
 using DentalClinic.Persistence.UnitsOfWork;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -12,13 +13,16 @@ using System.Threading.Tasks;
 
 namespace DentalClinic.Persistence
 {
-    public class RegisterPersistenceServices
+    public static class RegisterPersistenceServices
     {
-        public static IServiceCollection AddPersistenceServices(IServiceCollection services) 
+        public static IServiceCollection AddPersistenceServices(this IServiceCollection services) 
         {
+            var sp = services.BuildServiceProvider();
+            var configuration = sp.GetRequiredService<IConfiguration>();
+
             services.AddDbContext<DentalClinicDbContext>(options =>
             {
-                options.UseSqlServer("DentalClinicDbConnectionString");
+                options.UseSqlServer(configuration.GetConnectionString("DentalClinicDb"));
             });
 
             services.AddScoped<IDentalOfficeRepository, DentalOfficeRepository>();
