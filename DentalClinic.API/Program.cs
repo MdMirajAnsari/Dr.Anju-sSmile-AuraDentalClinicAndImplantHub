821d3b2084214f1a2f2ef2ed04ebe086d7167064
Miraj <Miraj@DESKTOP-PIQRSMH>
2026-03-12 12:50:03 +0530

    feat: swagger integrated

--- DentalClinic.API/Program.cs
+++ DentalClinic.API/Program.cs
@@ -6,6 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddPersistenceServices();
 builder.Services.AddApplicationServices();
 
@@ -14,6 +17,17 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
+
+// Enable middleware to serve generated Swagger as a JSON endpoint and the Swagger UI.
+if (app.Environment.IsDevelopment() || true)
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "DentalClinic API v1");
+        c.RoutePrefix = string.Empty; // serve UI at app root
+    });
+}
 
 app.UseAuthorization();
 
